1957b48f4da70c75cbb881782454e494f5d9b45b
HoangCV <cvhoangpt@gmail.com>
2019-11-07 22:50:46 +0700

    Thêm phương thức CSDL

---
+++ eclipse_code/Quanlikhobai/src/database/DB_Connector.java
@@ -0,0 +1,50 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DB_Connector 
+{
+	private static String DB_URL = "";
+	private static String USERNAME = "root";
+	private static String PASSWORD = "";
+	
+	public static Connection getConnection(String dbURL, String userName, String passWord)
+	{
+		Connection conn = null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, userName, passWord);
+			System.out.println("Connect successfully ...");
+		} catch (Exception e)
+		{
+			System.out.println("Connect failure.");
+            e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void main(String args[]) 
+	{
+		try {
+           
+            Connection conn = getConnection(DB_URL, USERNAME, PASSWORD);
+            // crate statement
+            Statement stmt = conn.createStatement();
+            // get data from table 'student'
+            ResultSet rs = stmt.executeQuery("select * from student");
+            // show data
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) 
+                        + "  " + rs.getString(3));
+            }
+            // close connection
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
